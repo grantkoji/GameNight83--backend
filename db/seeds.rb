5e6513b298da2e999df6fa6e21fa314d84930333
grantkoji <grantkoji@gmail.com>
2020-08-05 10:59:07 -0400

    with Chinese Poker created

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,25 @@ end
     Friendship.create!(user_id: user1, friend_id: user2)
 end
 
-Game.create!(title: "C Poker", creator_id: User.all.sample.id, )
+# create_table "games", force: :cascade do |t|
+#     t.string "title"
+#     t.integer "creator_id"
+#     t.integer "min_num_players"
+#     t.integer "max_num_players"
+#     t.integer "min_age"
+#     t.string "description"
+#     t.string "instructions_and_rules"
+#     t.string "image_url", default: ""
+#     t.string "link_to_game_website", default: ""
+#     t.string "game_category"
+#   end
+Game.create!(title: "Chinese Poker", creator_id: User.all.sample.id, min_age: 12, min_num_players: 2, max_num_players: 4, image_url: "https://pnimg.net/w/articles/0/564/a36c0b4689.jpg", description: "A combination"\
+" of Poker and Switch", link_to_game_website: "https://www.888poker.com/magazine/poker-world/intro-to-chinese-poker", category: "card", rules: "Every player is dealt 13 cards. The goal is to get rid of all of your cards"\
+" first. The player holding the 3 of clubs will go first. The first player can play one card, a pair, or a five card hand. The next player can either pass their turn or play a matching hand (single card, pair, or five card hand) with a higher value. Values of the"\
+" cards are 2 is the highest, then Ace, then King and down in order until you get to 3. Suits are also ranked with spade the highest, then heart, then diamond, then club. So a 7 of spades is higher than a 7 of hearts. Players continue"\
+" to play a matching hand with a higher value until everyone else in the game has passed. If everyone else has passed, whoever played the last hand will start the next one.")
+
+
 
 100.times do 
     rating = rand(1..10)
