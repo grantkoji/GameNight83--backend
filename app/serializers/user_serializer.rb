7bf056b1f2ed541e9531f8d89a651686efa5c398
grantkoji <grantkoji@gmail.com>
2020-08-08 15:53:33 -0400

    with backend changes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :total_friends, :games
+  attributes :id, :name, :username, :profile_url, :age, :fav_games, :total_friends, :games
   has_many :reviews
   has_many :game_photos
 
 
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+
   namespace :api do
     namespace :v1 do
-      resources :users
+      get '/users', to: 'users#index'
+      post '/users', to: 'users#create'
       get '/users/stay_logged_in', to: 'users#stay_logged_in'
-      post '/users/login', to: 'users#login'
+      post '/user/login', to: 'users#login'
+      get '/users/:id', to: 'users#show'
+      put '/users/:id', to: 'users#update'
+      delete '/users/:id', to: 'users#destroy' 
     end
   end
+
+
+
   namespace :api do
     namespace :v1 do
       resources :friendships
