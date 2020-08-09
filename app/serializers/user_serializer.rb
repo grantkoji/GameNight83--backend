bb84d5a7ce270796e58a8f8c8095281916ae36b5
grantkoji <grantkoji@gmail.com>
2020-08-09 00:47:39 -0400

    with changes to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :profile_url, :age, :fav_games, :total_friends, :games
-  has_many :reviews
-  has_many :game_photos
-
-
+  attributes :id, :name, :username, :profile_url, :age, :fav_games, :total_friends
 end
 
 
