39aa9ec54960c52d153ba67705bd287390b5fd99
grantkoji <grantkoji@gmail.com>
2020-08-11 21:32:28 -0400

    with changes

--- app/serializers/friendship_serializer.rb
+++ app/serializers/friendship_serializer.rb
@@ -1,3 +1,13 @@
 class FriendshipSerializer < ActiveModel::Serializer
   attributes :id, :friend1_name, :user_id, :friend_id, :friend2_name
+
+  # Nested Array
+  # class UserSerializer < ActiveModel::Serializer
+  #   has_many :friendships
+  
+  #   class FriendshipSerializer < ActiveModel::Serializer
+  #     attributes :id, :friend1_name, :friend2_name
+  #   end
+  
+  # end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :username, :profile_url, :age, :fav_games, :total_friends
+  attributes :id, :name, :username, :profile_url, :age, :fav_games, :total_friends, :friend_requests_sent, :friend_requests_received
 end
 
 
